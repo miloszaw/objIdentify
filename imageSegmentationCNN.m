52ffdb56c437b59a84e5c57452f60c31d903f64e
TrustworthyBlake <andreasblakli@gmail.com>
2021-04-09 23:26:53 +0200

    Changed readImgFilesFromFolder function to go through a folder containing images and resize them to fit with the CNN model, then depending on the operator variable it will either write the the new images to disk or return as a variable, renamed the function aswell. CNN changed to work with the new labels from PixelLabelData_2, CNN allows to input an image the variable C, its then put through the CNN, semantic segmentation labels the different regions of the banana with different colors to indicate which pixels fits the best with the labels

--- imageSegmentationCNN.m
+++ imageSegmentationCNN.m
@@ -3,14 +3,19 @@ clc;
 main();
 
 function main()
-%readImgFilesFromFolder();
-cnn();
+    path = '.\bananasImgs\';
+    %[~,~] = readWriteImgFilesFromToFolder(path, 1);
+    cnn();
 end
 
-function [allImg, numOfFiles] = readImgFilesFromFolder(path, fileType)
-    %path = '.\day01\';
-    destFolder = '.\resizedImages';
-    switch fileType
+% Reads images from file, writes resized images to folder, specify function
+% variable "operation" for what you need the function to do
+% 1 = read, resisze and write to disk
+% 2 = read, resize, put all img in variable which is returned as allImg
+function [allImg, numOfFiles] = readWriteImgFilesFromToFolder(path, operation)
+    dayCount = 0;
+    destFolder = '.\resizedDatasetBananas2';
+    switch operation
         case 1
             imgFile = dir([path '*.jpg']);
             if ~exist(destFolder, 'dir')
@@ -24,12 +29,16 @@ function [allImg, numOfFiles] = readImgFilesFromFolder(path, fileType)
         currFilename = imgFile(i).name;
         currImg = imread([path currFilename]);
         currImg = currImg(:,:,min(1:3, end));
-        %allImg{i} = imresize(currImg, [224 224]);
-        currImg = imresize(currImg, [224 224]);
-        switch fileType
+        currImg = imresize(currImg, [224 224]);     % Image resized to 224x224x3
+        switch operation
             case 1
-                if ~exist(destFolder, 'dir')
-                    imgString = append(destFolder, '\', currFilename, 'Resized.jpg');
+                if exist(destFolder, 'dir')
+                    if(mod(i-1, 10) == 0)
+                        dayCount = dayCount + 1;
+                    end
+                    countStr = num2str(i);                  % This is retarded
+                    dayCountStr = num2str(dayCount);
+                    imgString = append(destFolder, '\', 'banana', countStr, '_day', dayCountStr, '_Resized.jpg');
                     imwrite(currImg, imgString);                    
                 end
                 allImg = 0;
@@ -39,30 +48,21 @@ function [allImg, numOfFiles] = readImgFilesFromFolder(path, fileType)
     end
 end
 
-function data = customReadDatastoreImage(filename)  % code from matlab forum
-% code from default function: 
-onState = warning('off', 'backtrace'); 
-c = onCleanup(@() warning(onState)); 
-data = imread(filename); % added lines: 
-data = data(:,:,min(1:3, end)); 
-data = imresize(data,[224 224]);
-end
-
 function cnn()
     path = '.\day01\';
     pathIm = '.\test3imgtrain\';
-    pathImgDS = '.\resizedImages';
-    pathLab = '.\PixelLabelData_1\';
+    pathImgDS = '.\resizedDatasetBananas';%'.\resizedImages';
+    pathLab = '.\PixelLabelData_2\';%'.\PixelLabelData_1\';
     dataSetDir = fullfile(pathImgDS);
     imgDir = fullfile(dataSetDir, '*.jpg');
     labelDir = fullfile(pathLab, '*.png');
     imds = imageDatastore(imgDir);
     %imds = imresize(imdsTemp, [32, 32]);
     
-    [allImgTemp, ~] = readImgFilesFromFolder(pathIm, 1);
+    %[allImgTemp, ~] = readWriteImgFilesFromToFolder(pathIm, 1);
     %imds = imageDatastore('.\resizedImages', '*.jpg' );
     
-    classNames = ["apple", "banana", "orange"];
+    classNames = ["normalbanana", "badbanana", "background"];
     labelIDs   = [1 2 3];
     pxds = pixelLabelDatastore(labelDir, classNames, labelIDs);   
     
@@ -89,12 +89,13 @@ function cnn()
 
     trainingData = pixelLabelImageDatastore(imds,pxds);
     net = trainNetwork(trainingData,layers,opts);
-    [testImage, ~] = readImgFilesFromFolder(path, 2);
+    [testImage, ~] = readWriteImgFilesFromToFolder(pathImgDS, 2);
     
-    C = semanticseg(testImage{1},net);
-    B = labeloverlay(testImage{1},C);
+    C = semanticseg(testImage{98},net);
+    B = labeloverlay(testImage{98},C);%, 'IncludedLabels', "banana", 'Colormap','autumn','Transparency',0.25);
     imshow(B)
-    
+    %tempLabelImg = imcrop(testImage{1}, B);
+    %imshow(tempLabelImg);
 end
 
 % function segmentImageLazysnap()
@@ -110,47 +111,4 @@ end
 %     background = createMask(b1,I{1}) + createMask(b2,I{1});
 %     BW = lazysnapping(I{1},L,foreground,background);
 %     imshow(labeloverlay(I{1},BW,'Colormap',[0 1 0]))
-% end
-% 
-% function temp()
-% 	inputSize = [32 32 3];
-%     imgLayer = imageInputLayer(inputSize);
-%     filterSize = 3;
-%     numFilters = 32;
-%     conv = convolution2dLayer(filterSize,numFilters,'Padding',1);
-%     relu = reluLayer();
-%     poolSize = 2;
-%     maxPoolDownsample2x = maxPooling2dLayer(poolSize,'Stride',2);
-%     downsamplingLayers = [
-%         conv
-%         relu
-%         maxPoolDownsample2x
-%         conv
-%         relu
-%         maxPoolDownsample2x
-%     ]
-%     filterSize = 4;
-%     transposedConvUpsample2x = transposedConv2dLayer(4,numFilters,'Stride',2,'Cropping',1);
-%     upsamplingLayers = [
-%         transposedConvUpsample2x
-%         relu
-%         transposedConvUpsample2x
-%         relu
-%     ]
-%     numClasses = 3;
-%     conv1x1 = convolution2dLayer(1,numClasses);    
-%     finalLayers = [
-%         conv1x1
-%         softmaxLayer()
-%         pixelClassificationLayer()
-%     ]
-% 
-%     net = [
-%         imgLayer    
-%         downsamplingLayers
-%         upsamplingLayers
-%         finalLayers
-%         %opts
-%         ]
-%     
 % end
